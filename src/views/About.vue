28abaf5776a93f349bf3e06b9d418e150f2c392b
fanqiang <1327912591@qq.com>
2020-12-18 18:05:44 +0800

    ..

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="about">
+  <div class='about'>
     <h1>This is an about page</h1>
+    <input id='fileId2' type='file' multiple='multiple' name='file' />
   </div>
 </template>
